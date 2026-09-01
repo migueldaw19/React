a39a3de0ad2e9e6c27e3b4c5699817ee2690b2bb
Miguel_arc99 <55553794+migueldaw19@users.noreply.github.com>
2026-09-01 18:31:27 +0200

    Curso anterior

--- 3-counter-app/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { CounterApp } from './CounterApp'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <CounterApp value={ 7 } />
-  </React.StrictMode>
-)
